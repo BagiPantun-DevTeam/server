94b88830390d37698416354b0f6ce22d07b7e0a1
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-19 20:42:57 +0700

    installed bcryptjs

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const user = require('../models/user');
+
+class UserController {
+  static addNewUser(req, res) {
+    
+    user.create({
+
+    })
+  },
+}
