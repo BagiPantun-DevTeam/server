42d3022505a1b2ccc2d6889652f386f4ad9ad305
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-19 20:47:19 +0700

    added addUser function to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 const user = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 class UserController {
   static addNewUser(req, res) {
-    
-    user.create({
-
-    })
-  },
+    bcrypt.hash(req.body.password, 8, function (err, hash) {
+      if (err) {
+        res
+          .status(400)
+          .send(err);
+      } else {
+        user.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: hash,
+        }, function (response) {
+          if (err) {
+            res
+              .status(400)
+              .send(err);
+          } else {
+            res
+              .status(200);
+          }
+        });
+      }
+    });
+  }
 }
