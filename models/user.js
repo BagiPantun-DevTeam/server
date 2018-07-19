8964b6b94bfb48b0b00e9bf7f0922c19ce140a31
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-19 20:32:26 +0700

    finished User schema

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Book',
+  }]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
