a1c3f02e55c0ea6b3bbdc43a38a24d993d69ce85
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-19 20:48:53 +0700

    configured user router for addNewUser functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  usersRouter.addNewUser(req, res);
 });
 
 module.exports = router;
