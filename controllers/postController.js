11dbf8a72998341ebeeea36730a8da6ba96d9014
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-19 22:05:02 +0700

    finished addNewPost API

---
+++ controllers/postController.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const post = require('../models/post');
+
+class PostController {
+  static addNewPost(req, res) {
+    post.create({
+      content: req.body.content,
+      imageSource: req.body.imageSource,
+      userId: req.body.userId,
+    })
+    .then((response) => {
+      res
+        .status(200)
+        .send(response);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send(err);
+    });
+  }
+}
+
+module.exports = PostController;
