351daa05f13054bd6a15151b8b463d7ebfa90826
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-20 10:46:41 +0700

    edit user.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const userController = require('../controllers/userController');
 const Middleware = require('../middleware/token');
 const tokenMiddleware = Middleware.verifyToken;
-<<<<<<< HEAD
 const tumblrController = require("../controllers/tumblrController")
 
 router.post('/signup', userController.signUp);
@@ -11,11 +10,9 @@ router.post('/signin', userController.signIn);
 router.post('/quotes',tokenMiddleware, tumblrController.postQuote);
 router.post('/text',tokenMiddleware,tumblrController.postText);
 router.post('/image',tokenMiddleware,tumblrController.postPicture);
-=======
 
 router.post('/signup', userController.signUp);
 router.post('/signin', userController.signIn);
->>>>>>> 2967702e83858f3f706c040ef0143b33cea25489
 
 // router.post('/signin')
 
