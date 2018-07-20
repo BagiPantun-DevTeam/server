b9db5bc385e025ce6040663979c144e765be979d
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-20 13:25:40 +0700

    added isomorphic fetch package

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/gcs', gcsRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-app.use('unsplash', unsplashRouter);
+app.use('/unsplash', unsplashRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/unsplash.js
+++ routes/unsplash.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const router = express.router();
+var express = require('express');
+var router = express.Router();
+const Unsplash = require('unsplash-js').default;
+const dotenv = require('dotenv');
 
-router.get("/", function (req, re) {
+const unsplash = new Unsplash({
+  applicationId: process.env.APP_ACCESS_KEY,
+  secret: process.env.APP_SECRET,
+  callbackUrl: process.env.CALLBACK_URL,
+});
 
+router.get('/', function (req, res) {
+  unsplash.photos.getRandomPhoto({ collections: [2385134] })
+  .then((photoData) => {
+    res
+      .send(photoData)
+      .status(200);
+  })
+  .catch((err) => {
+    res
+      .send(err)
+      .status(400);
+  })
+  // .then(json => {
+  //
+  // });
 });
 
 module.exports = router;
